a971c3cf97ffbd581f8304f18336a885a018d849
ramya1102 <ramyapanditi@gmail.com>
2023-10-14 22:04:29 +0530

    first commit

---
+++ src/colors.js
@@ -0,0 +1,20 @@
+const colorArray = [
+    { id: 1, color: "#fcba03" },
+    { id: 2, color: "#3366FF" },
+    { id: 3, color: "#66FF33" },
+    { id: 4, color: "#FF33F6" },
+    { id: 5, color: "#33FF99" },
+    { id: 6, color: "#FF9933" },
+    { id: 7, color: "#3399FF" },
+    { id: 8, color: "#9933FF" },
+    { id: 9, color: "#33FF33" },
+    { id: 10, color: "#FF3366" },
+    { id: 11, color: "#030ffc" },
+    { id: 12, color: "#e7fc03" },
+    { id: 13, color: "#03fcba" },
+    { id: 14, color: "#03cefc" },
+    { id: 15, color: "#365d63" },
+  ];
+  
+export default colorArray;
+  
